bab9bdaa7e11ca192d137114b2374ba934e74c65
Petre Leonard Macamete <petreleonardos@gmail.com>
2022-06-06 20:47:17 +0300

    Add project files.

---
+++ Roweb test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Roweb_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
